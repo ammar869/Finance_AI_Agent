c7990ae95da24921c68101cee23f339db412db41
ammar869 <ammarhassan1131318@gmail.com>
2026-08-12 02:54:44 +0500

    file and folder issue

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from finance-ai-agent!")
+from llm.llm_file import llm
+from tools.currency import get_exchange_rate
 
+llm_with_tools = llm.bind_tools([get_exchange_rate])
 
-if __name__ == "__main__":
-    main()
+response = llm_with_tools.invoke(
+    "What is the current USD to PKR exchange rate?"
+)
+
+print(response)
