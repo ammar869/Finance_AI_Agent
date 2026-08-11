db78b32778c07049655a45c577bdc293cd1e19ac
ammar869 <ammarhassan1131318@gmail.com>
2026-08-11 20:13:19 +0500

    making simple function

--- src/tools/currency.py
+++ src/tools/currency.py
@@ -1,4 +1,17 @@
 
 from dotenv import load_dotenv
 import requests
+import os
 
+load_dotenv()
+def get_exchange_rate(base_currency, target_currency):
+   api_key = os.environ.get("EXCHANGE_API_KEY")
+   response = requests.get(f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{base_currency}")
+   data = response.json()
+   if data["result"] == "success":
+         exchange_rate = data["conversion_rates"][target_currency]
+         return exchange_rate
+   else:
+         raise Exception("Error fetching exchange rate")
+   
+get_exchange_rate("PKR", "USD")
