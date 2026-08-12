0a67db340a5148a6bdb185d0243346de6d262460
ammar869 <ammarhassan1131318@gmail.com>
2026-08-12 15:26:00 +0500

    making percentage tool

---
+++ src/tools/percentage.py
@@ -0,0 +1,6 @@
+from langchain_core.tools import tool
+
+@tool
+def calculate_percentage(value: float, percentage: float) -> float:
+    """Calculate a percentage of a given value."""
+    return value * (percentage / 100)
