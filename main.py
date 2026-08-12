210e40d0ef7ff951bb7737d67032dfa7c1142e12
ammar869 <ammarhassan1131318@gmail.com>
2026-08-12 17:14:26 +0500

    multi tool calling

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ from src.tools.currency import get_exchange_rate
 from src.tools.percentage import calculate_percentage
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_core.messages import HumanMessage, ToolMessage
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
 
+db = SQLDatabase.from_uri("sqlite:///finance.db")
+
+toolkit = SQLDatabaseToolkit(
+    db=db,
+    llm=llm
+)
+sql_tools = toolkit.get_tools()
+
+for tool in sql_tools:
+    print(tool.name)
 
 searching_tool = DuckDuckGoSearchRun()
 # Available tools
 tools = [
     get_exchange_rate,
     calculate_percentage,
-    searching_tool
+    searching_tool,
+    *sql_tools
 ]
 
 # Tool name → actual tool
@@ -25,8 +38,8 @@ llm_with_tools = llm.bind_tools(tools)
 
 
 # User asks a question
-user_message = "Latest news about Pakistan, turkey, Saudia"
-
+#user_message = "Latest news about Pakistan, turkey, Saudia"
+user_message = "How much money was spent on Food?"
 response = llm_with_tools.invoke(user_message)
 
 print("Tool calls:")
@@ -34,16 +47,25 @@ print(response.tool_calls)
 
 
 # Get the tool call selected by the LLM
-tool_call = response.tool_calls[0]
+#tool_call = response.tool_calls[0]
 
 
 # Find the correct tool
-tool = tool_map[tool_call["name"]]
+#tool = tool_map[tool_call["name"]]
 
 
 # Execute the correct tool
-tool_result = tool.invoke(tool_call["args"])
+#tool_result = tool.invoke(tool_call["args"])
 
+for tool_call in response.tool_calls:
+
+    tool = tool_map[tool_call["name"]]
+
+    tool_result = tool.invoke(tool_call["args"])
+
+    print(tool_call["name"])
+    print(tool_result)
+    
 print("Tool result:")
 print(tool_result)
 
